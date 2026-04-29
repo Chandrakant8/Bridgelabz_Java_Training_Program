637073469597c8a3e408040fcad30d0b9e0dd784
Chandrakant8 <yadavchandrakant874@gmail.com>
2026-04-29 16:01:41 +0530

    Added HarryAge program

---
+++ src/Core_Programming_Basics/Programming_Elements/Level1/HarryAge.java
@@ -0,0 +1,9 @@
+package Core_Programming_Basics.Programming_Elements.Level1;
+public class HarryAge{
+	public static void main(String[] args) {
+		int birthYear = 2000;
+		int currentYear = 2024;
+		int age = currentYear - birthYear;
+		System.out.println("Harry's age in 2024 is " + age);
+	}
+}
