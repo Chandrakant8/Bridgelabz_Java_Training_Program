6c4e20dcda1abed161f1e7401498378a435076be
Chandrakant8 <yadavchandrakant874@gmail.com>
2026-04-29 16:44:30 +0530

    Added DividePens program

---
+++ src/Core_Programming_Basics/Programming_Elements/Level1/DividePens.java
@@ -0,0 +1,10 @@
+package Core_Programming_Basics.Programming_Elements.Level1;
+public class DividePens{
+	public static void main(String[] args) {
+		int totalPen = 14;
+		int totalStudent = 3;
+		int dist= 14/3;
+		int notDist = 14%3;
+		System.out.printf("The Pen Per Student is %d and the remaining pen not distributed is %d", dist , notDist);
+	}
+}
