0080de038a6ea6382158965f8b6c102020ec30d6
Chandrakant8 <yadavchandrakant874@gmail.com>
2026-04-30 12:00:24 +0530

    Added SideOfSquare program

---
+++ src/Core_Programming_Basics/Programming_Elements/Level1/SideOfSquare.java
@@ -0,0 +1,12 @@
+package Core_Programming_Basics.Programming_Elements.Level1;
+
+import java.util.Scanner;
+
+public class SideOfSquare{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		float perimeter = sc.nextFloat();
+		float side = perimeter / 4f;
+		System.out.println("The length of the side is "+ side +" whose perimeter is "+ perimeter);
+	}
+}
