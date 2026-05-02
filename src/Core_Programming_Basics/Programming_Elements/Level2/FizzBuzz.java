c26b8d55b25bc985740b08f138704d7ae9c8c47f
Chandrakant8 <yadavchandrakant874@gmail.com>
2026-05-03 00:21:03 +0530

    Added FizzBuzz program

--- src/Core_Programming_Basics/Programming_Elements/Level2/FizzBuzz.java
+++ src/Core_Programming_Basics/Programming_Elements/Level2/FizzBuzz.java
@@ -10,7 +10,7 @@ public class FizzBuzz{
 			if(i>0 && i%3==0 && i%5==0)  System.out.println("FizzBuzz");
 			else if(i>0 && i%3==0) System.out.println("Fizz");
 			else if(i>0 && i%5==0) System.out.println("Buzz");
-			else  System.out.println(i);
+			else System.out.println(i);
 		}
 	}
 }
