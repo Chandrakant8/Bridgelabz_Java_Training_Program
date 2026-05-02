a4469b3ffbe025ccbfdc7e37c41ffad66dcb9439
Chandrakant8 <yadavchandrakant874@gmail.com>
2026-05-02 23:54:53 +0530

    Added FizzBuzz Program

--- src/Core_Programming_Basics/Programming_Elements/Level2/FizzBuzz.java
+++ src/Core_Programming_Basics/Programming_Elements/Level2/FizzBuzz.java
@@ -6,9 +6,11 @@ public class FizzBuzz{
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int value = sc.nextInt();
-		if(value>0 && value%3==0 && value%5==0)  System.out.println("FizzBuzz");
-		else if(value>0 && value%3==0) System.out.println("Fizz");
-		else if(value>0 && value%5==0) System.out.println("Buzz");
-		else  System.out.println(value);
+		for(int i=1 ; i<=value ; i++) {			
+			if(i>0 && i%3==0 && i%5==0)  System.out.println("FizzBuzz");
+			else if(i>0 && i%3==0) System.out.println("Fizz");
+			else if(i>0 && i%5==0) System.out.println("Buzz");
+			else  System.out.println(i);
+		}
 	}
 }
