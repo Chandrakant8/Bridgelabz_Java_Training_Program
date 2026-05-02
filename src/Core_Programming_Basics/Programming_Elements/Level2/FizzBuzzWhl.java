674b94d2bcf09707ec86fcf0a1739b29cddac553
Chandrakant8 <yadavchandrakant874@gmail.com>
2026-05-03 00:20:21 +0530

    Added FizzBuzzWhl program

---
+++ src/Core_Programming_Basics/Programming_Elements/Level2/FizzBuzzWhl.java
@@ -0,0 +1,18 @@
+package Core_Programming_Basics.Programming_Elements.Level2;
+
+import java.util.Scanner;
+
+public class FizzBuzzWhl{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int value = sc.nextInt();
+		int temp = 1;
+		while(value>0 && temp<=value) {
+			if(temp%3==0 && temp%5==0)  System.out.println("FizzBuzz");
+			else if(temp%3==0) System.out.println("Fizz");
+			else if(temp%5==0) System.out.println("Buzz");
+			else System.out.println(temp);
+			temp++;
+		}
+	}
+}
