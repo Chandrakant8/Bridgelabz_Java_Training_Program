1862b0b4b9506e1173b3670536fdf46724e91560
Chandrakant8 <yadavchandrakant874@gmail.com>
2026-05-05 12:44:21 +0530

    Added CheckNumber program

---
+++ src/Core_Programming_Basics/Control_Flow/Level1/CheckNumber.java
@@ -0,0 +1,13 @@
+package Core_Programming_Basics.Control_Flow.Level1;
+
+import java.util.*;
+
+public class CheckNumber{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		if(num<0) System.out.println("Negative");
+		else if(num==0) System.out.println("Zero");
+		else System.out.println("Positive");
+	}
+}
