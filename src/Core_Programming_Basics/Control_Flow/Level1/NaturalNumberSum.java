32310a4c82fe4ccb5421cab9a01aaff22cd2268c
Chandrakant8 <yadavchandrakant874@gmail.com>
2026-05-05 15:04:11 +0530

    Added NaturalNumberSum program

---
+++ src/Core_Programming_Basics/Control_Flow/Level1/NaturalNumberSum.java
@@ -0,0 +1,18 @@
+package Core_Programming_Basics.Control_Flow.Level1;
+
+import java.util.Scanner;
+ 
+public class NaturalNumberSum{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		int calcSum = 0;
+		int temp = number;
+		while(temp>0) {
+			calcSum+=temp;
+			temp--;
+		}
+		int sumByFormula = number*(number+1)/2;
+		System.out.print(calcSum+" "+sumByFormula);
+	}
+}
