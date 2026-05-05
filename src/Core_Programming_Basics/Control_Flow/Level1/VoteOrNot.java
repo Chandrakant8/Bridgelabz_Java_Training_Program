e47115638ed1477593017f02dfbf200a989c54fb
Chandrakant8 <yadavchandrakant874@gmail.com>
2026-05-05 12:05:34 +0530

    Added a person can vote or not program

---
+++ src/Core_Programming_Basics/Control_Flow/Level1/VoteOrNot.java
@@ -0,0 +1,16 @@
+package Core_Programming_Basics.Control_Flow.Level1;
+
+import java.util.*;
+
+public class VoteOrNot{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int age = sc.nextInt();
+		if(age>=18) {
+			System.out.println("The person can vote");
+		}
+		else {
+			System.out.println("The person cannot vote");
+		}
+	}
+}
