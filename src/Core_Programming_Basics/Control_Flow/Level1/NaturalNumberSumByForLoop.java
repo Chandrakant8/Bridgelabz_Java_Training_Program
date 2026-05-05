1faba7d46adf12888c09055adaf11cb250141966
Chandrakant8 <yadavchandrakant874@gmail.com>
2026-05-05 15:07:39 +0530

    Added NaturalNumberSumByForLoop program

---
+++ src/Core_Programming_Basics/Control_Flow/Level1/NaturalNumberSumByForLoop.java
@@ -0,0 +1,16 @@
+package Core_Programming_Basics.Control_Flow.Level1;
+
+import java.util.Scanner;
+ 
+public class NaturalNumberSumByForLoop{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		int calcSum = 0;
+		for(int i=1 ; i<=number ; i++) {
+			calcSum+=i;
+		}
+		int sumByFormula = number*(number+1)/2;
+		System.out.print(calcSum+" "+sumByFormula);
+	}
+}
