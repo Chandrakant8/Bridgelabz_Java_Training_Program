dbde14955edd90bec0fb39ced1bdd37f92b4b813
Chandrakant8 <yadavchandrakant874@gmail.com>
2026-06-09 11:12:50 +0530

    Implement SRP that ensure to focus on single responsibility or task.

--- src/DesignPrinciples/Solid/SingleResponsibility.java
+++ src/DesignPrinciples/Solid/SingleResponsibility.java
@@ -1,29 +1,34 @@
 package DesignPrinciples.Solid;
 
+//class for Baking Bread
 class BreadBaker{
 	public void bakeBread() {
 		System.out.println("Baking high quality bread...");
 	}
 }
 
+// class for Managing Inventory
 class InventoryManager{
 	public void manageInventory() {
 		System.out.println("Managing inventory...");
 	}
 }
 
+// class for Supply Order
 class SupplyOrder{
 	public void orderSupplies() {
 		System.out.println("Ordering supplies...");
 	}
 }
 
+// class for Customer Service
 class CustomerService{
 	public void serveCustomer() {
 		System.out.println("Serving customer...");
 	}
 }
 
+// class for Bakery Cleaner
 class BakeryCleaner{
 	public void cleanBakery() {
 		System.out.println("Cleaning the bakery...");
@@ -32,6 +37,9 @@ class BakeryCleaner{
 
 class SingleResponsibility{
 	public static void main(String[] args) {
+		
+		// here instance of each class that shows only one responsibility of each class
+		
 		BreadBaker b = new BreadBaker();
 		InventoryManager m = new InventoryManager();
 		SupplyOrder s = new SupplyOrder();
